2fe774297bed8c4868b870f399b21eb12bf9bba9
Krysson <mgulla@krysson.com>
2024-07-03 14:45:41 -0400

    Post lesson # 18 pre Lesson # 19

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-function ExampleComponenet() {
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+
+function Main() {
   return (
-    <div>
-      <h1>This is Our app!!!</h1>
-      <p>The sky is blue and the grass is green</p>
-    </div>
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
   )
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"))
-root.render(<ExampleComponenet />)
+root.render(<Main />)
 
 if (module.hot) {
   module.hot.accept()
